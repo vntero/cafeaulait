e61df6abd2dff6ab3c817c05925dabed8a8ba2e1
vntero <shin.antero@gmail.com>
2024-07-29 13:01:23 +0200

    attempts to fix 2 footers showing up

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+// cafeaulait corporate colours
+// ---- orange, rgba (255, 75, 57, 1)
+// ---- beige, FFF1DB
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,3 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store'
 
-export const openModal = writable(false);
-
-// cafeaulait corporate colours
-// ---- orange, rgba (255, 75, 57, 1)
-// ---- beige, FFF1DB
+export const openModal = writable(false)
