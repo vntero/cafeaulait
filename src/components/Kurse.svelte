58be562c5a18b6bc5cd7b1c3d5ce9ae4717d7339
vntero <shin.antero@gmail.com>
2024-08-22 18:52:26 +0200

    starts kurse section revamp

--- src/components/Kurse.svelte
+++ src/components/Kurse.svelte
@@ -1,26 +1,5 @@
 <script>
-  import KurseCard from './KurseCard.svelte'
   import SectionWrapper from './SectionWrapper.svelte'
-  import { onMount, onDestroy } from 'svelte'
-  let kurseFeatures = [
-    {
-      featureList: [],
-      description:
-        'Lerne in deiner Altersgruppe panafrikanische Tänze, Afro Beats, Perkussionsrhythmen und Gesänge kennen und mach mit bei unseren Shows. Café au Lait begleitet dich von den ersten Tanzschritten bis zur Bühnenreife.',
-      imgUrl: 'assets/afrotanz.png',
-    },
-  ]
-
-  let currentIndex = 0
-  let intervalId
-
-  onMount(() => {
-    intervalId = setInterval(() => {
-      currentIndex = (currentIndex + 1) % kurseFeatures.length
-    }, 2000)
-  })
-
-  onDestroy(() => clearInterval(intervalId))
 </script>
 
 <SectionWrapper id="kurse">
@@ -32,24 +11,5 @@
         Kurse
       </h3>
     </div>
-    {#each kurseFeatures as kurseFeature, index}
-      <KurseCard {kurseFeature} {index}>
-        {#if index === 0}
-          Unsere Tanz-, Trommel und Gesangskurse – <span
-            class="text-red-500 font-medium"
-          >
-            ab 6 Jahren</span
-          >
-        {:else if index === 1}
-          Afro <span class="text-red-500 font-medium">Beats</span>
-        {:else if index === 2}
-          Afro Beats <span class="text-red-500 font-medium">Masterclass</span>
-        {:else if index === 3}
-          Gospel<span class="text-red-500 font-medium">chor</span>
-        {:else}
-          Stage <span class="text-red-500 font-medium">Company</span>
-        {/if}
-      </KurseCard>
-    {/each}
   </div>
 </SectionWrapper>
