e708ff99df57dbd8ccdd7596d38c260027a37dbb
vntero <shin.antero@gmail.com>
2024-08-27 09:26:49 +0200

    removes all image files

---
+++ src/components/Book.svelte
@@ -0,0 +1,15 @@
+<script>
+  import BookModal from './BookModal.svelte'
+  let showBookModal = false
+
+  const handleBookClose = () => {
+    showBookModal = false
+  }
+</script>
+
+<div class="flex items-center gap-4">
+  <button class="specialBtn" on:click={() => (showBookModal = true)}>
+    <p class="text-base sm:text-lg md:text-xl">Buche eine Show</p>
+  </button>
+  <BookModal show={showBookModal} on:close={handleBookClose} />
+</div>
