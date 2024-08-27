b37779d51801605d733ee44109b90b0e323abd6c
vntero <shin.antero@gmail.com>
2024-08-27 14:27:28 +0200

    install daisyui

---
+++ src/routes/produktionen/+page.svelte
@@ -0,0 +1,15 @@
+<div class="diff aspect-[16/9]">
+  <div class="diff-item-1">
+    <div
+      class="bg-primary text-primary-content grid place-content-center text-9xl font-black"
+    >
+      DAISY
+    </div>
+  </div>
+  <div class="diff-item-2">
+    <div class="bg-base-200 grid place-content-center text-9xl font-black">
+      DAISY
+    </div>
+  </div>
+  <div class="diff-resizer"></div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 }
