48f84132f0caa9af27777341e5d7f64b8bdc0975
vntero <shin.antero@gmail.com>
2024-08-27 16:42:41 +0200

    adds carousel with le souffle photos, this time with the correct format

--- src/routes/produktionen/+page.svelte
+++ src/routes/produktionen/+page.svelte
@@ -43,48 +43,27 @@
     </div>
 
     <!-- CAROUSEL -->
-    <div class="carousel rounded-box max-w-[1000px] space-x-4">
+    <div class="carousel rounded-box space-x-2 h-64 max-w-[1000px]">
       <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-          alt="Burger"
-        />
+        <img src="assets/ls1.png" alt="Burger" />
       </div>
       <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-          alt="Burger"
-        />
+        <img src="assets/ls2.png" alt="Burger" />
       </div>
       <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-          alt="Burger"
-        />
+        <img src="assets/ls3.png" alt="Burger" />
       </div>
       <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-          alt="Burger"
-        />
+        <img src="assets/ls4.png" alt="Burger" />
       </div>
       <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
-          alt="Burger"
-        />
+        <img src="assets/ls5.png" alt="Burger" />
       </div>
       <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
-          alt="Burger"
-        />
+        <img src="assets/ls6.png" alt="Burger" />
       </div>
       <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-          alt="Burger"
-        />
+        <img src="assets/ls7.png" alt="Burger" />
       </div>
     </div>
     <!-- PRODUCTION DESCRIPTION -->
