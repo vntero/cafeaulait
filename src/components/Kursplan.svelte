c9fff8060b018444e18baf114884b12a4dfcfe3d
vntero <shin.antero@gmail.com>
2024-08-28 14:36:06 +0200

    adds kursplan modal

---
+++ src/components/Kursplan.svelte
@@ -0,0 +1,15 @@
+<script>
+  import KursplanModal from './Kursplan.svelte'
+  let showKursplanModal = false
+
+  const handleKursplanClose = () => {
+    showKursplanModal = false
+  }
+</script>
+
+<div class="flex items-center gap-4">
+  <button class="specialBtn" on:click={() => (showKursplanModal = true)}>
+    <p class="text-base sm:text-lg md:text-xl">Buche eine Show</p>
+  </button>
+  <KursplanModal show={showKursplanModal} on:close={handleKursplanClose} />
+</div>
