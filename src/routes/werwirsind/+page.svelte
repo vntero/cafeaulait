f8e84c28cc27bb93d6a2ada70fbdd64d7898a983
vntero <shin.antero@gmail.com>
2024-10-25 12:24:55 +0200

    adds urbain's last name

--- src/routes/werwirsind/+page.svelte
+++ src/routes/werwirsind/+page.svelte
@@ -45,7 +45,7 @@
       img: '/assets/sekou.png',
     },
     {
-      name: 'Urbain Nachname',
+      name: 'Urbain Guiguembe',
       role: 'Tanzlehrerin',
       img: '/assets/urbain.png',
     },
@@ -149,9 +149,11 @@
                   </div>
                   <h6 class="text-xl font-semibold">Wer wir sind</h6>
                   <p class="mt-2 mb-4 text-blueGray-500">
-                    Wir sind ein Zürcher Verein für Afrotanz, Gesang, Schauspiel &
-                    Perkussion. Unser Angebot ist allen Kindern zugänglich unabhängig von ihrer Hautfarbe, Herkunft und Konfession.
-                    Unsere Arbeit ist beim Sportamt Zürich als Jugendsportförderung anerkannt.
+                    Wir sind ein Zürcher Verein für Afrotanz, Gesang, Schauspiel
+                    & Perkussion. Unser Angebot ist allen Kindern zugänglich
+                    unabhängig von ihrer Hautfarbe, Herkunft und Konfession.
+                    Unsere Arbeit ist beim Sportamt Zürich als
+                    Jugendsportförderung anerkannt.
                   </p>
                 </div>
               </div>
@@ -170,8 +172,11 @@
                   </div>
                   <h6 class="text-xl font-semibold">Unser Ziel</h6>
                   <p class="mt-2 mb-4 text-blueGray-500">
-                    Mit unserem Angebot wollen wir die Diversität der Schweizer Gesellschaft sichtbar machen und einen Beitrag zu mehr Toleranz und Dialog leisten. 
-                    Unsere Produktionen behandeln deshalb gesellschaftskritische Themen und im Unterricht lernen die Schüler:innen spielerisch Respekt, gegenseitiges 
+                    Mit unserem Angebot wollen wir die Diversität der Schweizer
+                    Gesellschaft sichtbar machen und einen Beitrag zu mehr
+                    Toleranz und Dialog leisten. Unsere Produktionen behandeln
+                    deshalb gesellschaftskritische Themen und im Unterricht
+                    lernen die Schüler:innen spielerisch Respekt, gegenseitiges
                     Verständnis und Solidarität.
                   </p>
                 </div>
@@ -192,8 +197,10 @@
                   <h6 class="text-xl font-semibold">Woher wir kommen</h6>
 
                   <p class="mt-2 mb-4 text-blueGray-500">
-                     Café au Lait wurde 2003 von der gebürtigen Angolanerin, Françoise Strassburg, gegründet. Seither haben unsere Pädagog:innen bereits mehrere hundert 
-                     Kinder von ihren ersten Tanzschritten bis hin zur Bühnenreife begleitet. 
+                    Café au Lait wurde 2003 von der gebürtigen Angolanerin,
+                    Françoise Strassburg, gegründet. Seither haben unsere
+                    Pädagog:innen bereits mehrere hundert Kinder von ihren
+                    ersten Tanzschritten bis hin zur Bühnenreife begleitet.
                   </p>
                 </div>
               </div>
@@ -201,48 +208,6 @@
           </div>
         </div>
       </section>
-    </div>
-  </div>
-</SectionWrapper>
-
-<!-- our kids -->
-<SectionWrapper>
-  <div
-    class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center"
-  >
-    <div class="flex flex-col gap-2">
-      <h3
-        class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center"
-      >
-        Unser <span class="text-red-500">Nachwuchs</span>
-      </h3>
-    </div>
-    <div
-      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-10 md:gap-14 w-full"
-    >
-      {#each kids as kid}
-        <div
-          class="flex flex-col gap-2 text-left max-w-[400px] w-full mx-auto relative p-4 px-6"
-        >
-          <div
-            class="absolute top-0 left-0 w-1/3 h-[1px] bg-slate-950 -translate-x-4"
-          ></div>
-          <div
-            class="absolute top-0 left-0 h-2/3 w-[1px] bg-slate-950 -translate-y-4"
-          ></div>
-          <div class="flex items-center gap-4">
-            <img
-              src={kid.img}
-              alt={kid.name}
-              class="w-20 h-20 rounded-full object-cover shadow-md"
-            />
-            <h4 class="text-lg sm:text-xl md:text-2xl pt-2 text-red-500">
-              {kid.name}
-            </h4>
-          </div>
-          <p class="pl-2">{kid.age}, {kid.origin}</p>
-        </div>
-      {/each}
     </div>
   </div>
 </SectionWrapper>
@@ -289,4 +254,45 @@
   </div>
 </SectionWrapper>
 
+<!-- our kids -->
+<SectionWrapper>
+  <div
+    class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center"
+  >
+    <div class="flex flex-col gap-2">
+      <h3
+        class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center"
+      >
+        Unser <span class="text-red-500">Nachwuchs</span>
+      </h3>
+    </div>
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-10 md:gap-14 w-full"
+    >
+      {#each kids as kid}
+        <div
+          class="flex flex-col gap-2 text-left max-w-[400px] w-full mx-auto relative p-4 px-6"
+        >
+          <div
+            class="absolute top-0 left-0 w-1/3 h-[1px] bg-slate-950 -translate-x-4"
+          ></div>
+          <div
+            class="absolute top-0 left-0 h-2/3 w-[1px] bg-slate-950 -translate-y-4"
+          ></div>
+          <div class="flex items-center gap-4">
+            <img
+              src={kid.img}
+              alt={kid.name}
+              class="w-20 h-20 rounded-full object-cover shadow-md"
+            />
+            <h4 class="text-lg sm:text-xl md:text-2xl pt-2 text-red-500">
+              {kid.name}
+            </h4>
+          </div>
+          <p class="pl-2">{kid.age}, {kid.origin}</p>
+        </div>
+      {/each}
+    </div>
+  </div>
+</SectionWrapper>
 <Footer />
