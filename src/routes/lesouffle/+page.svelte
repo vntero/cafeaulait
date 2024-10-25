dba21fe23c7cb1c30310fed165d3cd6621eb38da
vntero <shin.antero@gmail.com>
2024-10-25 14:36:40 +0200

    uses existing style for le souffle title

--- src/routes/lesouffle/+page.svelte
+++ src/routes/lesouffle/+page.svelte
@@ -31,7 +31,7 @@
   <Header />
   <div class="flex flex-col gap-10 flex-1 items-center mt-16">
     <!-- LE SOUFFLE - DIFF SLIDER TITLE -->
-    <div
+    <!-- <div
       class="diff aspect-[16/2] rounded-lg max-w-[1000px] mx-auto w-full text-center"
     >
       <div class="diff-item-1">
@@ -49,6 +49,14 @@
         </div>
       </div>
       <div class="diff-resizer"></div>
+    </div> -->
+    <div class="flex flex-col gap-2 mt-20">
+      <p class="opacity-60 text-base sm:text-lg md:text-xl text-center"></p>
+      <h3
+        class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center"
+      >
+        Le <span class="text-red-500">Souffle</span>
+      </h3>
     </div>
 
     <!-- SHORT DESCRIPTION AND PRODUCER -->
