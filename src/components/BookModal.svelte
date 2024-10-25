9416bf6b7fe47ee6233189059de51a3d88f0d192
vntero <shin.antero@gmail.com>
2024-10-25 11:40:23 +0200

    adds new field to book modal

--- src/components/BookModal.svelte
+++ src/components/BookModal.svelte
@@ -81,7 +81,7 @@
             <!-- CONTACT DETAILS -->
             <h6 class="mt-3 mb-6 font-bold uppercase">Kontaktdaten</h6>
             <div class="flex flex-wrap">
-              <div class="w-full lg:w-4/12 px-4">
+              <div class="w-full lg:w-3/12 px-4">
                 <div class="relative w-full mb-3">
                   <label
                     class="block text-sm font-semibold mb-2"
@@ -98,7 +98,7 @@
                   />
                 </div>
               </div>
-              <div class="w-full lg:w-4/12 px-4">
+              <div class="w-full lg:w-3/12 px-4">
                 <div class="relative w-full mb-3">
                   <label class="block text-sm font-semibold mb-2" for="phone">
                     Telefonnummer <span class="text-red-500">*</span>
@@ -112,7 +112,7 @@
                   />
                 </div>
               </div>
-              <div class="w-full lg:w-4/12 px-4">
+              <div class="w-full lg:w-3/12 px-4">
                 <div class="relative w-full mb-3">
                   <label class="block text-sm font-semibold mb-2" for="email">
                     Emailadresse <span class="text-red-500">*</span>
@@ -123,6 +123,19 @@
                     type="email"
                     class="border-0 px-3 py-3 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                     required
+                  />
+                </div>
+              </div>
+              <div class="w-full lg:w-3/12 px-4">
+                <div class="relative w-full mb-3">
+                  <label class="block text-sm font-semibold mb-2" for="email">
+                    Organisation
+                  </label>
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    class="border-0 px-3 py-3 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                   />
                 </div>
               </div>
